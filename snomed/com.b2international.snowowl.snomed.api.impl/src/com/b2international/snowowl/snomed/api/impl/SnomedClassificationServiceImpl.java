3a1583305d2300fcc59ce116436c050086250a4f
Gabor Nagy <gnagy@b2international.com>
2017-08-14 14:10:50 +0200

    Revert "CLASS-4: Add configuration for an external classification service"
    This reverts commit a821520f9084f3c6414a884243094e9c955074ff.
    
    Conflicts:
    	snomed/com.b2international.snowowl.snomed.api.impl/src/com/b2international/snowowl/snomed/api/impl/SnomedClassificationServiceImpl.java

--- snomed/com.b2international.snowowl.snomed.api.impl/src/com/b2international/snowowl/snomed/api/impl/SnomedClassificationServiceImpl.java
+++ snomed/com.b2international.snowowl.snomed.api.impl/src/com/b2international/snowowl/snomed/api/impl/SnomedClassificationServiceImpl.java
@@ -532,7 +532,7 @@ public class SnomedClassificationServiceImpl implements ISnomedClassificationSer
 	private Integer getMaxReasonerRuns() {
 		if (maxReasonerRuns == null) {
 			maxReasonerRuns = ApplicationContext.getInstance().getServiceChecked(SnowOwlConfiguration.class)
-					.getModuleConfig(SnomedCoreConfiguration.class).getClassificationConfig().getMaxReasonerRuns();
+					.getModuleConfig(SnomedCoreConfiguration.class).getMaxReasonerRuns();
 		}
 		return Preconditions.checkNotNull(maxReasonerRuns, "maximum number of reasoner runs must be configured");
 	}
