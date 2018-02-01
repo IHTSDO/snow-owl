11a0ddc8624231af57040c7e6e81f17a41b41c7a
Zoltan Molnar <zmolnar@b2international.com>
2018-02-01 14:51:30 +0100

    ATF-1191 Added JsonUnwrapped annotations for the CSV parser.

--- snomed/com.b2international.snowowl.snomed.api.rest/src/com/b2international/snowowl/snomed/api/rest/domain/SnomedConceptMini.java
+++ snomed/com.b2international.snowowl.snomed.api.rest/src/com/b2international/snowowl/snomed/api/rest/domain/SnomedConceptMini.java
@@ -1,15 +1,19 @@
 package com.b2international.snowowl.snomed.api.rest.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
 
 public class SnomedConceptMini {
-
+	@JsonUnwrapped
+	@JsonProperty("id")
 	private String id;
+	@JsonUnwrapped()
+	@JsonProperty("fsn")
 	private String fsn;
 	
 	public SnomedConceptMini(String id) {
 		this(id, id);
 	}
-	
 	public SnomedConceptMini(String id, String fsn) {
 		this.id = id;
 		this.fsn = fsn;
