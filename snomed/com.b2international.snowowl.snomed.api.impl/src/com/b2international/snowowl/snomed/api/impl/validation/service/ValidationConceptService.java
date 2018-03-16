92349118e1ce8d87e2b9e54df6b96934c0dde78b
Quyen Ly <quyen.ly@huntermacdonald.com>
2018-03-16 10:09:53 +0700

    APDS-288 Drools Rules [new]: Concept parents in more than one top-level hierarchy

--- snomed/com.b2international.snowowl.snomed.api.impl/src/com/b2international/snowowl/snomed/api/impl/validation/service/ValidationConceptService.java
+++ snomed/com.b2international.snowowl.snomed.api.impl/src/com/b2international/snowowl/snomed/api/impl/validation/service/ValidationConceptService.java
@@ -1,8 +1,16 @@
 package com.b2international.snowowl.snomed.api.impl.validation.service;
 
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.ihtsdo.drools.domain.Concept;
 import org.ihtsdo.drools.service.ConceptService;
 
 import com.b2international.snowowl.eventbus.IEventBus;
+import com.b2international.snowowl.snomed.SnomedConstants.Concepts;
+import com.b2international.snowowl.snomed.core.domain.SnomedConcept;
+import com.b2international.snowowl.snomed.core.domain.SnomedConcepts;
 import com.b2international.snowowl.snomed.datastore.SnomedDatastoreActivator;
 import com.b2international.snowowl.snomed.datastore.request.SnomedRequests;
 
@@ -23,6 +31,36 @@ public class ValidationConceptService implements ConceptService {
 				.execute(bus)
 				.getSync()
 				.isActive();
+	}
+
+	@Override
+	public Set<String> findStatedAncestorsOfConcept(Concept concept) {
+		
+		return Collections.emptySet();
+	}
+
+	@Override
+	public Set<String> findTopLevelHierachiesOfConcept(Concept concept) {
+		SnomedConcepts concepts = SnomedRequests.prepareSearchConcept()
+				.filterByEcl(">" + concept.getId() + " AND <!" + Concepts.ROOT_CONCEPT)
+				.filterByActive(true)
+				.build(SnomedDatastoreActivator.REPOSITORY_UUID, branchPath)
+				.execute(bus)
+				.getSync();
+			
+		return concepts.getItems().stream().map(SnomedConcept::getId).collect(Collectors.toSet());
+	}
+
+	@Override
+	public Set<String> getAllTopLevelHierachies() {
+		SnomedConcepts concepts = SnomedRequests.prepareSearchConcept()
+				.filterByEcl("<!" + Concepts.ROOT_CONCEPT)
+				.filterByActive(true)
+				.build(SnomedDatastoreActivator.REPOSITORY_UUID, branchPath)
+				.execute(bus)
+				.getSync();
+			
+		return concepts.getItems().stream().map(SnomedConcept::getId).collect(Collectors.toSet());
 	}
 
 }
