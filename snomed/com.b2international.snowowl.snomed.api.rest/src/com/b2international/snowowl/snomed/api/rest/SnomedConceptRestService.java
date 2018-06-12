aec62b9b6816561f603de6df53b9ecbc8ab73371
Peter Groves Williams <petergwilliams@hotmail.com>
2018-06-12 10:49:51 +0100

    STORM-72 Removed 'normalise attribute values' flag from Authoring Form endpoint.
    It had no function on the back end, remove to avoid confusion with Snowstorm counterpart

--- snomed/com.b2international.snowowl.snomed.api.rest/src/com/b2international/snowowl/snomed/api/rest/SnomedConceptRestService.java
+++ snomed/com.b2international.snowowl.snomed.api.rest/src/com/b2international/snowowl/snomed/api/rest/SnomedConceptRestService.java
@@ -366,10 +366,6 @@ public class SnomedConceptRestService extends AbstractSnomedRestService {
 			@PathVariable(value="conceptId")
 			final String conceptId,
 			
-			@ApiParam(value="Normalise attribute values")
-			@RequestParam(value="normaliseAttributeValues", defaultValue="true")
-			final boolean normaliseAttributeValues,
-			
 			@ApiParam(value="Accepted language tags, in order of preference")
 			@RequestHeader(value=HttpHeaders.ACCEPT_LANGUAGE, defaultValue="en-US;q=0.8,en-GB;q=0.6", required=false) 
 			final String acceptLanguage) {
