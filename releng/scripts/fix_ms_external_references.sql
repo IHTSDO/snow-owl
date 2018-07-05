fcb5331c5dce350045eebda3b7ed03e593a46267
Gabor Nagy <gnagy@b2international.com>
2018-07-05 14:52:29 +0200

    [ms] Share scripts and files that were necessary to fix international...
    ... content in MS before the 2018-07-31 upgrade

---
+++ releng/scripts/fix_ms_external_references.sql
@@ -0,0 +1,26 @@
+USE snomedStore;
+
+INSERT INTO cdo_external_refs VALUES (-161, "http://b2international.com/snowowl/snomed/refset/1.0#//SnomedAnnotationRefSetMember", -1);
+INSERT INTO cdo_external_refs VALUES (-162, "http://b2international.com/snowowl/snomed/refset/1.0#//SnomedAnnotationRefSetMember/annotation", -1);
+INSERT INTO cdo_external_refs VALUES (-163, "http://b2international.com/snowowl/snomed/refset/1.0#//SnomedMRCMDomainRefSetMember", -1);
+INSERT INTO cdo_external_refs VALUES (-164, "http://b2international.com/snowowl/snomed/refset/1.0#//SnomedMRCMDomainRefSetMember/domainConstraint", -1);
+INSERT INTO cdo_external_refs VALUES (-165, "http://b2international.com/snowowl/snomed/refset/1.0#//SnomedMRCMDomainRefSetMember/parentDomain", -1);
+INSERT INTO cdo_external_refs VALUES (-166, "http://b2international.com/snowowl/snomed/refset/1.0#//SnomedMRCMDomainRefSetMember/proximalPrimitiveConstraint", -1);
+INSERT INTO cdo_external_refs VALUES (-167, "http://b2international.com/snowowl/snomed/refset/1.0#//SnomedMRCMDomainRefSetMember/proximalPrimitiveRefinement", -1);
+INSERT INTO cdo_external_refs VALUES (-168, "http://b2international.com/snowowl/snomed/refset/1.0#//SnomedMRCMDomainRefSetMember/domainTemplateForPrecoordination", -1);
+INSERT INTO cdo_external_refs VALUES (-169, "http://b2international.com/snowowl/snomed/refset/1.0#//SnomedMRCMDomainRefSetMember/domainTemplateForPostcoordination", -1);
+INSERT INTO cdo_external_refs VALUES (-170, "http://b2international.com/snowowl/snomed/refset/1.0#//SnomedMRCMDomainRefSetMember/editorialGuideReference", -1);
+INSERT INTO cdo_external_refs VALUES (-171, "http://b2international.com/snowowl/snomed/refset/1.0#//SnomedMRCMAttributeDomainRefSetMember", -1);
+INSERT INTO cdo_external_refs VALUES (-172, "http://b2international.com/snowowl/snomed/refset/1.0#//SnomedMRCMAttributeDomainRefSetMember/domainId", -1);
+INSERT INTO cdo_external_refs VALUES (-173, "http://b2international.com/snowowl/snomed/refset/1.0#//SnomedMRCMAttributeDomainRefSetMember/grouped", -1);
+INSERT INTO cdo_external_refs VALUES (-174, "http://b2international.com/snowowl/snomed/refset/1.0#//SnomedMRCMAttributeDomainRefSetMember/attributeCardinality", -1);
+INSERT INTO cdo_external_refs VALUES (-175, "http://b2international.com/snowowl/snomed/refset/1.0#//SnomedMRCMAttributeDomainRefSetMember/attributeInGroupCardinality", -1);
+INSERT INTO cdo_external_refs VALUES (-176, "http://b2international.com/snowowl/snomed/refset/1.0#//SnomedMRCMAttributeDomainRefSetMember/ruleStrengthId", -1);
+INSERT INTO cdo_external_refs VALUES (-177, "http://b2international.com/snowowl/snomed/refset/1.0#//SnomedMRCMAttributeDomainRefSetMember/contentTypeId", -1);
+INSERT INTO cdo_external_refs VALUES (-178, "http://b2international.com/snowowl/snomed/refset/1.0#//SnomedMRCMAttributeRangeRefSetMember", -1);
+INSERT INTO cdo_external_refs VALUES (-179, "http://b2international.com/snowowl/snomed/refset/1.0#//SnomedMRCMAttributeRangeRefSetMember/rangeConstraint", -1);
+INSERT INTO cdo_external_refs VALUES (-180, "http://b2international.com/snowowl/snomed/refset/1.0#//SnomedMRCMAttributeRangeRefSetMember/attributeRule", -1);
+INSERT INTO cdo_external_refs VALUES (-181, "http://b2international.com/snowowl/snomed/refset/1.0#//SnomedMRCMAttributeRangeRefSetMember/ruleStrengthId", -1);
+INSERT INTO cdo_external_refs VALUES (-182, "http://b2international.com/snowowl/snomed/refset/1.0#//SnomedMRCMAttributeRangeRefSetMember/contentTypeId", -1);
+INSERT INTO cdo_external_refs VALUES (-183, "http://b2international.com/snowowl/snomed/refset/1.0#//SnomedMRCMModuleScopeRefSetMember", -1);
+INSERT INTO cdo_external_refs VALUES (-184, "http://b2international.com/snowowl/snomed/refset/1.0#//SnomedMRCMModuleScopeRefSetMember/mrcmRuleRefsetId", -1);
